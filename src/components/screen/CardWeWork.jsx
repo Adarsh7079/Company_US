99eb16e60ca6fcb987b71c79126ebd1ebac55bcf
Adarsh7079 <adarshk8271@gmail.com>
2024-06-25 20:31:59 +0530

    second

--- src/components/screen/CardWeWork.jsx
+++ src/components/screen/CardWeWork.jsx
@@ -36,6 +36,33 @@ const CardWeWork = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+          
         
       };
   return (
--- src/components/screen/MainScreen.jsx
+++ src/components/screen/MainScreen.jsx
@@ -1,76 +1,78 @@
 import React from "react";
-import {CardWeWork} from "../index"
+import { CardWeWork } from "../index";
 
 const MainScreen = () => {
   return (
-    <div className=" mt-20">
+    <div className="mt-20">
       {/* Section 1 */}
-      <section className="px-10 w-2/3">
-        <div className=" text-8xl ">
-          We’re ready to share our deepest expertice in
+      <section className="px-4 sm:px-10 w-full sm:w-2/3">
+        <div className="text-4xl sm:text-8xl">
+          We’re ready to share our deepest expertise in
         </div>
       </section>
 
       {/* Section 2 */}
-      <section className=" mx-10 mt-[12rem]">asdasd</section>
+      <section className="mx-4 sm:mx-10 mt-12 sm:mt-[12rem]">asdasd</section>
 
-      {/* section 3 */}
-      <section className=" w-5/6 mt-28 flex mx-auto ">
-        <div className=" text-[52px] w-[80%] leading-[75px]">
+      {/* Section 3 */}
+      <section className="w-full sm:w-5/6 mt-16 sm:mt-28 flex mx-auto">
+        <div className="text-2xl sm:text-[52px] w-full sm:w-[80%] leading-tight sm:leading-[75px]">
           MediaSoft Solutions is a software development company that provides
           full cycle of a product development from the scratch.
         </div>
       </section>
 
       {/* Section 4 */}
-      <section className=" w-5/6 mt-32 flex mx-auto ">
-        <div className="  w-full flex justify-between">
-          <div className=" text-3xl w-[500px] leading-[45px]">
+      <section className="w-full sm:w-5/6 mt-20 sm:mt-32 flex flex-col sm:flex-row mx-auto">
+        <div className="w-full sm:w-auto flex flex-col sm:flex-row justify-between">
+          <div className="text-xl sm:text-3xl leading-tight sm:leading-[45px] mb-6 sm:mb-0 sm:w-[500px]">
             Check out our Services to learn more about what we can do for you.
           </div>
-          <div className=" flex flex-col ">
-            <div className=" flex flex-col  h-[250px]">
-              <h1 className=" text-[55px] text-orange-600 font-semibold">
+          <div className="flex flex-col sm:flex-row flex-wrap sm:space-x-6">
+            <div className="flex flex-col h-auto sm:h-[250px] mb-6 sm:mb-0">
+              <h1 className="text-3xl sm:text-[55px] text-orange-600 font-semibold">
                 200+ Projects
               </h1>
-              <h1 className="w-[300px] text-xl leading-[40px]">
+              <h1 className="w-full sm:w-[300px] text-lg sm:text-xl leading-tight sm:leading-[40px]">
                 completed for a number of businesses from e-commerce, retail,
                 travel and insurance
               </h1>
             </div>
-            <div className=" flex flex-col h-[250px]">
-              <h1 className=" text-[55px] text-orange-600 font-semibold">
+            <div className="flex flex-col h-auto sm:h-[250px] mb-6 sm:mb-0">
+              <h1 className="text-3xl sm:text-[55px] text-orange-600 font-semibold">
                 13+ Awards
               </h1>
-              <h1 className="w-[300px] text-xl leading-[40px]">
+              <h1 className="w-full sm:w-[300px] text-lg sm:text-xl leading-tight sm:leading-[40px]">
                 from various exhibitions and conferences
               </h1>
             </div>
-            <div className=" flex flex-col h-[250px]">
-              <h1 className=" text-[55px] text-orange-600 font-semibold">80+</h1>
-              <h1 className="w-[300px] text-xl leading-[40px]">
+            <div className="flex flex-col h-auto sm:h-[250px] mb-6 sm:mb-0">
+              <h1 className="text-3xl sm:text-[55px] text-orange-600 font-semibold">80+</h1>
+              <h1 className="w-full sm:w-[300px] text-lg sm:text-xl leading-tight sm:leading-[40px]">
                 highly experienced professionals
               </h1>
             </div>
-          </div>
-          <div className=" flex flex-col">
-            <div className=" flex flex-col h-[250px]">
-              <h1 className=" text-[55px] text-orange-600 font-semibold">
+            <div className="flex flex-col h-auto sm:h-[250px] mb-6 sm:mb-0">
+              <h1 className="text-3xl sm:text-[55px] text-orange-600 font-semibold">
                 10+ Years
               </h1>
-              <h1 className="w-[300px] text-xl leading-[40px]">of expertise</h1>
+              <h1 className="w-full sm:w-[300px] text-lg sm:text-xl leading-tight sm:leading-[40px]">
+                of expertise
+              </h1>
             </div>
-            <div className=" flex flex-col h-[250px]">
-              <h1 className=" text-[55px] text-orange-600 font-semibold">50+</h1>
-              <h1 className="w-[300px] text-xl leading-[40px]">
+            <div className="flex flex-col h-auto sm:h-[250px]">
+              <h1 className="text-3xl sm:text-[55px] text-orange-600 font-semibold">50+</h1>
+              <h1 className="w-full sm:w-[300px] text-lg sm:text-xl leading-tight sm:leading-[40px]">
                 delighted customers
               </h1>
             </div>
           </div>
         </div>
       </section>
-      <section>
-        <CardWeWork/>
+
+      {/* CardWeWork Section */}
+      <section className="mt-16 sm:mt-32">
+        <CardWeWork />
       </section>
     </div>
   );
